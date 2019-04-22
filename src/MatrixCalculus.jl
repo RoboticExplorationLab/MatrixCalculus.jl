ad1853ad74e53b0b5cbdbe767e3a9d67efdd0a72
Brian Jackson <bjack205@gmail.com>
2019-04-22 14:34:23 -0700

    Finish implementation

--- src/MatrixCalculus.jl
+++ src/MatrixCalculus.jl
@@ -1,11 +1,14 @@
 module MatrixCalculus
 using LinearAlgebra
 using SparseArrays
+using DocStringExtensions
 
-greet() = print("Hello World!")
-
+"$(SIGNATURES) Create a sparse identity matrix"
 speye(n::Int) = sparse(Diagonal(1I,n))
-function comm(n::Int, m::Int, sparse::Bool=true)
+
+"$(SIGNATURES) Create a commutator matrix C_(n,m) such that vec(A') = C*vec(A) where A in an n×m matrix
+Will default to a sparse matrix if n*m > 100, or the `sparse` flag is set to `true` "
+function comm(n::Int, m::Int, sparse::Bool=false)
     if sparse || n*m > 100
         reshape(kron(speye(n)[:], speye(m)), n*m, n*m)
     else
@@ -13,10 +16,31 @@ function comm(n::Int, m::Int, sparse::Bool=true)
     end
 end
 
-vech(S) = view(S,triinds(S))
+"$(SIGNATURES) Returns the half-vectorization of a symmetric matrix as a view of the original matrix.
+It is a vector of stacked columns of the lower triangular portion of S"
+vech(S) = view(S,trilinds(S))
+"$(SIGNATURES) Returns the half-vectorization of a symmetric matrix, given a previous half-vectorization of the same size"
 vech(S,S2::SubArray) = view(S,S2.indices[1])
 
-function triinds!(inds,A)
+"$(SIGNATURES) Calculate the linear indices of the lower triangular portion of a square matrix"
+function trilinds!(inds,A)
+    n = size(A,1)
+    k = 1
+    for i = 1:n
+        inds[k:k+n-i] = (i:n) .+ (i-1)n
+        k += n-i+1
+    end
+    inds
+end
+function trilinds(A)
+    n = size(A,1)
+    inds = zeros(Int,(n+1)n÷2)
+    trilinds!(inds,A)
+    return inds
+end
+
+"$(SIGNATURES) Calculate the linear indices of the upper triangular portion of a square matrix"
+function triuinds!(inds,A)
     n = size(A,1)
     k = 1
     for i = 1:n
@@ -25,31 +49,27 @@ function triinds!(inds,A)
     end
     return inds
 end
-function triinds(A)
+function triuinds(A)
     n = size(A,1)
     inds = zeros(Int,(n+1)n÷2)
     triinds!(inds,A)
     return inds
 end
 
+"$(SIGNATURES) Returns the elimination matrix E_n such that vech(S) = E_n*vec(S).
+This is useful for taking derivatives with respect to a symmetric matrix"
 function elim(n)
     ns = (n+1)n÷2
     E = sparse(1:ns, Array(vech(reshape(1:(n*n),n,n))), ones(Int,ns));
 end
 
-function elim2(n)
-    ns = (n+1)n÷2
-    E = spzeros(Int,ns,n*n)
-    r,c = 1,1
-    for i = 1:n
-        E[r:r+i-1,c:c+i-1] = Diagonal(I,i)
-        r += i
-        c += n
-    end
-    return E
+"$(SIGNATURES) Returns the duplication matrix D_n such that vec(S) = D_n*vech(S).
+This is useful for taking derivatives with respect to a symmetric matrix"
+function dupl(n)
+    A = ones(Int,n,n)
+    A[trilinds(A)] = 1:(n+1)n÷2
+    A = Symmetric(A,:L)
+    D = sparse(1:(n*n), A[:], ones(Int,n*n))
 end
-
-density(A::SparseMatrixCSC) = nnz(A)/length(A)
-
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,59 +1,25 @@
 using Test
 using BenchmarkTools
-using BenchmarkTools
 
 n,m = rand(10:30,2)
+n,m = 5,3
 A = rand(n,m)
 vA = vec(A)
 C = comm(n,m,false)
-C == comm2(n,m)
 
 collect(1:m:n*m)
 @test C*vec(A) == vec(A')
 @test C*vA == vec(A')
 
-A = A'A
+A = A*A'
+ns = (n+1)n÷2
 S = vech(A)
-@test length(S) == (m+1)*(m/2)
+@test length(S) == ns
 vech(A,S)
 @test S[1] == A[1]
 @test S[2] == A[1,2]
-@test S[3] == A[2,2]
+@test S[n+1] == A[2,2]
 @test S[end] == A[end,end]
 
-
-inds = vcat(collect.(([(1:i) .+ (i-1)m for i = 1:m]))...)
-(m+1)*m/2
-vech2(A)
-vech3(A)
-S = vech3(A)
-@btime vech($A)
-@btime vech2($A)
-@btime vech3($A)
-@btime vech($A,$S)
-
-D = Diagonal(I,10)
-sparse(D)
-kron(sparsevec(D),sparse(D))
-
-D = Diagonal(I,100)
-@btime sparse($D)
-
-@btime spdiagm(0=>ones(100))
-@btime sparse(Diagonal(I,100))
-D = sparse(Diagonal(I,100))
-vec(D)
-sparsevec(D1)
-
-D1 = speye(n)
-D2 = speye(m)
-@btime reshape(kron($D1[:],$D2),n*m,n*m)
-@btime reshape(kron(sparsevec($D1),$D2),n*m,n*m)
-
-
-kron(vec(D1),D2)
-kron(sparsevec(D1),D2)
-kron(vec(Matrix(1I,n,n)),D2)
-sparsevec(D1) == vec(D1)
-Matrix(I,n,n)
-D1
+E = elim(n)
+@test E*vec(A) == vech(A)
